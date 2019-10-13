91fdb21659db281ee8abd52451de260102163a74
Pradeep Ummiti <pradeepummiti@yahoo.com>
2019-10-13 18:58:42 -0400

    Add script.js
    Syntax for variables to define numbers, strings, undefined data types in JS

---
+++ Basics/script.js
@@ -0,0 +1,19 @@
+console.log('Hello World...!');
+
+var firstName = "Pradeep";
+console.log(firstName);
+
+var lastName = "Ummiti";
+console.log(lastName);
+
+var age = 29;
+console.log(age);
+
+var ageTrue = true;
+console.log(ageTrue);
+
+var job;
+console.log(job);
+
+job = "Project Engineer"
+console.log(job);
